60464983771668f37e82a235cc32f4c8215d0e85
Danwooh <agnelbs88@gmail.com>
2025-05-19 09:38:59 -0500

    Agrego el chat bot al dashboard dentro de views

--- views/home/dashboard.php
+++ views/home/dashboard.php
@@ -51,5 +51,11 @@
                     <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" class="imagenes--footer">
                 </a>
     </footer>
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  chat-title="Mark"
+  agent-id="d13b7d8d-a8d9-4b85-88c8-9858f780da52"
+  language-code="es"
+></df-messenger>
 </body>
 </html>
