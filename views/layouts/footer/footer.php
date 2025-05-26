6b7d2baa45768b36cba1e01dfada4adedc584c09
edwinson <camix24k@gmail.com>
2025-05-26 07:21:59 -0500

    dasboard y layouts

---
+++ views/layouts/footer/footer.php
@@ -0,0 +1,29 @@
+
+
+    </div>
+    <footer class="main-footer">
+        <div class="main-footer__content">
+            <p>&copy; <?php echo date('Y'); ?> Salud Benefit. Todos los derechos reservados.</p>
+            <div class="social-links">
+                <a href="#" class="social-link">
+                    <img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" alt="Facebook">
+                </a>
+                <a href="#" class="social-link">
+                    <img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter">
+                </a>
+                <a href="#" class="social-link">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram">
+                </a>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+    <df-messenger
+        chat-title="Mark"
+        agent-id="d13b7d8d-a8d9-4b85-88c8-9858f780da52"
+        language-code="es">
+    </df-messenger>
+</body>
+
+</html>
