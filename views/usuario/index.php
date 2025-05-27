2a4ebd7c4b216fedf6aa26c4e1a56b7c94892992
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-05-27 06:22:01 -0500

    a

--- views/usuario/index.php
+++ views/usuario/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../config/database.php';
+require_once '../../config/database.php';
 
 $sql = "SELECT * FROM tbl_usuarios";
 $stmt = $pdo->query($sql);
@@ -12,7 +12,7 @@ $usuarios = $stmt->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../assets/css/admin/usuario.css">
+    <link rel="stylesheet" href="../../assets/css/admin/usuario.css">
     <title>Crud Usuarios</title>
 </head>
 <body>
