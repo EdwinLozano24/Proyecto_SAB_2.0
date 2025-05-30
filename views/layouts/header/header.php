29fbc084e57d98df0ba5162c6213e0646b534372
Allen01x <mikerice8842+1@gmail.com>
2025-05-30 10:01:28 -0500

    Linkeado layout con sus estilos

--- views/layouts/header/header.php
+++ views/layouts/header/header.php
@@ -1,4 +1,3 @@
-
 <!-- header.php 
 // Este archivo contendrá el encabezado común para todas las páginas
 // Puedes incluirlo en tus vistas principales para mantener la consistencia en el diseño
@@ -10,12 +9,13 @@
 <head>
     <link rel="stylesheet" href="../cssLayouts/layout.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
 </head>
 
 <body>
     <header class="main-header">
         <div class="main-header__logo">
-            <img src=".././Assets/img/Logo.png" alt="Logo Salud Benefit">
+            <img src="../../../Assets/img/Logo.png" alt="Logo Salud Benefit">
         </div>
         <nav class="main-nav">
             <ul class="main-nav__list">
@@ -27,6 +27,7 @@
             </ul>
         </nav>
     </header>
-        
-    </body>
+
+</body>
+
 </html>
