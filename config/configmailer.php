6245cd1a6e16b5116184b639f9ad5cada609677a
edwinson <camix24k@gmail.com>
2025-07-02 07:48:23 -0500

    configmailer.php

---
+++ config/configmailer.php
@@ -0,0 +1,27 @@
+<?php
+  // define("HOST","sandbox.smtp.mailtrap.io");
+  // define("PORT","587");
+  // define("USERNAME","95c2e8ba8dc75d");
+  // define("PASSWORD","0963d29a9e3452");
+
+
+  define("HOST","smtp.gmail.com");
+  define("USERNAME","saludbenefitt@gmail.com");
+  define("PASSWORD","iuxw rbta laja xrav");
+
+
+  define("SMTP_SECURE","TLS");
+  define("TIEMPO_VIDA",time()+120);
+
+
+  // define("TIEMPO_VIDA",time()*60); 1 hora
+  // define("TIEMPO_VIDA",time()*24); 1 dia
+  // define("TIEMPO_VIDA",time()*7); una semana
+
+
+    // $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    // $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    // $mail->Username   = 'saludbenefitt@gmail.com';                     //SMTP username
+    // $mail->Password   = 'iuxw rbta laja xrav';                               //SMTP password
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    // $mail->Port       = 465; 
