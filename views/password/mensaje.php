289933886e4e97a3194afca87b1785cbe98f9762
edwinson <camix24k@gmail.com>
2025-08-06 07:34:04 -0500

    vistas completas reset pasword

--- views/password/mensaje.php
+++ views/password/mensaje.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+    $cssPath = $_SERVER['DOCUMENT_ROOT'] . '/proyecto_sab/assets/css/user/resetpasword.css';
+    $cssUrl = '/proyecto_sab/assets/css/user/resetpasword.css';
+    if (file_exists($cssPath)) {
+        echo '<link rel="stylesheet" href="' . $cssUrl . '">';
+    } else {
+        echo ' CSS File not fount at: ' . $cssPath . '';
+    }
+    ?>
 </head>
+
 <body>
-    <p><?= htmlspecialchars($mensaje) ?></p>
-<a href="index.php?action=showRecoverForm">Volver</a>
+    <main>
+        <div class="contenedor__todo">
+            <div class="contenedor__reset">
+                <form action="">
+                <h2><?= htmlspecialchars($mensaje) ?></h2>
+                <a href="index.php?action=showRecoverForm" class="link-login">Volver</a>
+                </form>
+            </div>
+        </div>
 </body>
+
 </html>
--- views/password/resetear_form.php
+++ views/password/resetear_form.php
@@ -1,6 +1,33 @@
-<h2>Restablecer contraseña</h2>
-<form method="POST" action="index.php?action=resetPassword">
-  <input type="hidden" name="token" value="<?= htmlspecialchars($token) ?>">
-  <input type="password" name="nueva_password" placeholder="Nueva contraseña" required>
-  <button type="submit">Cambiar contraseña</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php
+  $cssPath = $_SERVER['DOCUMENT_ROOT'] . '/proyecto_sab/assets/css/user/resetpasword.css';
+  $cssUrl = '/proyecto_sab/assets/css/user/resetpasword.css';
+  if (file_exists($cssPath)) {
+    echo '<link rel="stylesheet" href="' . $cssUrl . '">';
+  } else {
+    echo ' CSS File not fount at: ' . $cssPath . '';
+  }
+  ?>
+</head>
+
+<body>
+  <main>
+    <div class="contenedor__todo">
+      <div class="contenedor__reset">
+        <form method="POST" action="index.php?action=resetPassword">
+          <h2>Restablecer contraseña</h2>
+          <input type="hidden" name="token" value="<?= htmlspecialchars($token) ?>">
+          <input type="password" name="nueva_password" placeholder="Nueva contraseña" required>
+          <button type="submit">Cambiar contraseña</button>
+        </form>
+      </div>
+    </div>
+</body>
+
+</html>
