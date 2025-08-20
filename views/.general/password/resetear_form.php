eee1e96208687953a481462b5dda989948405ff6
edwinson <camix24k@gmail.com>
2025-08-20 08:03:15 -0500

    cambios jotingel

--- views/.general/password/resetear_form.php
+++ views/.general/password/resetear_form.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <?php
-  $cssPath = $_SERVER['DOCUMENT_ROOT'] . '/proyecto_sab/assets/css/user/resetpasword.css';
-  $cssUrl = '/proyecto_sab/assets/css/user/resetpasword.css';
+  $cssPath = $_SERVER['DOCUMENT_ROOT'] . '/assets/css/user/resetpasword.css';
+  $cssUrl = '/assets/css/user/resetpasword.css';
   if (file_exists($cssPath)) {
     echo '<link rel="stylesheet" href="' . $cssUrl . '">';
   } else {
