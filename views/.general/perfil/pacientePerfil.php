ce4245ede84ac092f687da6a39fe99b3c0328759
Angelo gonzalez <4198126@gmail.com>
2025-08-23 15:17:07 -0500

    Prueba a ver si hoy si funcionan los estilos del perfil, primero que se vea rojo el input, solo para confirmar que lo coja por lo menos ya que el otro no lo coge de ninguna manera

--- views/.general/perfil/pacientePerfil.php
+++ views/.general/perfil/pacientePerfil.php
@@ -19,19 +19,11 @@ function obtenerIniciales($nombreCompleto) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tu Perfil</title>
+    <link rel="stylesheet" href="/assets/css/user/userPerfil2.css">
     <!-- Fony Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <?php
-    $cssPath = $_SERVER['DOCUMENT_ROOT'] . '/assets/css/user/userPerfil2.css';
-    $cssUrl = '/assets/css/user/userPerfil2.css';
-    if (file_exists($cssPath)) {
-        echo '<link rel="stylesheet" href="' . $cssUrl . '">';
-    } else {
-        echo ' CSS File not fount at: ' . $cssPath . '';
-    }
-    ?>
 
 </head>
 
