072e62737e3e0927ce651dda895a15a95ab1386d
Angelo gonzalez <4198126@gmail.com>
2025-08-23 15:32:41 -0500

    creo que porfin hp

--- views/.general/perfil/pacientePerfil.php
+++ views/.general/perfil/pacientePerfil.php
@@ -19,7 +19,7 @@ function obtenerIniciales($nombreCompleto) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tu Perfil</title>
-    <link rel="stylesheet" href="/Assets/css/user/userPerfil2.css">
+    <link rel="stylesheet" href="/Assets/css/user/userPerfil.css">
     <!-- Fony Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
