d572e0464095fc63284005252fc4725e4bed0c02
Edwinson <camix24k@gmail.com>
2025-08-23 23:51:29 -0500

    cambios crear pqrs

--- views/.general/layoutsFinal/admin/nav.php
+++ views/.general/layoutsFinal/admin/nav.php
@@ -20,7 +20,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="/views/paciente/pqr/crearPQR.php" class="nav-link">
+                <a href="/views/paciente/pqr/creapqr2.php" class="nav-link">
                     <div class="nav-icon"></div>
                     Crear Pqrs
                 </a>
--- views/paciente/pqr/crearPQR.php
+++ views/paciente/pqr/crearPQR.php
@@ -1,24 +1,40 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/auth.php';
+requiereSesion();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/database.php';
+$pdo = conectarBD();
+$sql = "SELECT * FROM tbl_usuarios";
+$stmt = $pdo->query($sql);
+$usua = $stmt->fetchAll();
+
+$sql = "SELECT * FROM tbl_empleados
+    INNER JOIN tbl_usuarios ON empl_usuario = id_usuario";
+$stmt = $pdo->query($sql);
+$empl = $stmt->fetchAll();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crear PQR</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="/Assets/css/layoutFinal/paciente/layout1.css">
-    <
-        <?php
-        $cssPath = $_SERVER['DOCUMENT_ROOT'] . '/assets/css/pqr/pqrCrear.css';
-        $cssUrl = '/assets/css/pqr/pqrCrear.css';
-        if (file_exists($cssPath)) {
-            echo '<link rel="stylesheet" href="' . $cssUrl . '">';
-        } else {
-            echo ' CSS File not fount at: ' . $cssPath . '';
-        }
-        ?>
+    <title>Generar Pqrs - Sistema Odontológico</title>
+    <!-- CSS de Select2 -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <?php
+    $cssPath = $_SERVER['DOCUMENT_ROOT'] . '/Assets/css/pqr/pqrCrear2.css';
+    $cssUrl = '/Assets/css/pqr/pqrCrear2.css';
+    if (file_exists($cssPath)) {
+        echo '<link rel="stylesheet" href="' . $cssUrl . '">';
+    } else {
+        echo ' CSS File not fount at: ' . $cssPath . '';
+    }
+    ?>
+</head>
 
-        </head>
+
 
 <body>
     <?php
@@ -59,18 +75,32 @@
                             <input type="text" name="pqrs_asunto" id="pqrs_asunto" required>
                         </div>
 
-                        <div class="form-group full-width">
-                            <label for="pqrs_descripcion" id="form-label-descripcion">Descripcion</label>
-                            <textarea name="pqrs_descripcion" id="pqrs_descripcion" maxlength="255"
-                                placeholder="Escriba su Pqrs..."></textarea>
-                        </div>
+                        < <div class="form-group full-width">
+                        <label for="pqrs_descripcion">Descripcion<span class="required">*</span></label>
+                        <textarea name="pqrs_descripcion" id="pqrs_descripcion" maxlength="255"
+                            placeholder="Escriba su Pqrs..."></textarea>
                     </div>
-
                 </div>
-
-            </form>
-        </div>
+            </div>
+            <div class="button-group">
+                    <button type="button" class="btn-secondary" onclick="window.history.back()">
+                    ← Cancelar
+                </button>
+                <input type="submit" id="generar_cita" value="⚠️ Registrar Pqrs">
+            </div>
+        </form>
+    </div>
     </main>
+        <!-- JS de jQuery y Select2 -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <!-- <script src="script.js"></script> -->
+    <script>
+        $(document).ready(function () {
+            $('#pqrs_usuario').select2();
+            });
+
+    </script>
 
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/paciente/footer.php'); ?>
 </body>
