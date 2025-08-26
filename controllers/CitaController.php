4e3426d77f2c6011b896b306d8e1977888edf730
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-08-26 07:40:14 -0500

    Especialista Cita

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -248,7 +248,6 @@ class CitaController
     }
     public function especialistaCitaView($id_usuario)
     {
-        var_dump($id_usuario); exit;
         $id_especialista = $this->EspecialistaModel->findCita($id_usuario);
         var_dump($id_especialista); exit;
     }
