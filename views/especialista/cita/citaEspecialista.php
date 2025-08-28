213fcdb6d85e7213e8c33e2b24550ffab86d9630
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-08-28 14:54:46 -0500

    Prueba Cita CSS

--- views/especialista/cita/citaEspecialista.php
+++ views/especialista/cita/citaEspecialista.php
@@ -31,9 +31,9 @@ $nombreUsuario = $_SESSION['usuario']['usua_nombre'];
 </head>
 <body>
     <?php 
-        // include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/header.php');
-        // include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/nav.php');
-        // include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/asideCita.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/header.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/nav.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/asideCita.php');
     ?>
     <main class="main-content">
             <h2 class="content-title">Tus Citas Asignadas</h2>
@@ -81,7 +81,7 @@ $nombreUsuario = $_SESSION['usuario']['usua_nombre'];
         </table>
     </main>
     <?php
-        // include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/footer.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/footer.php');
     ?>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
