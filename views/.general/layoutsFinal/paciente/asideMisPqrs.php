a6a03dd465f2d3fd160526655bd5d4f62a81a5a4
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-08-28 20:38:40 -0500

    Prueba Cita CSS

---
+++ views/.general/layoutsFinal/paciente/asideMisPqrs.php
@@ -0,0 +1,17 @@
+<?php
+$id_usuario = $_SESSION['usuario']['id_usuario'];
+?>
+    <aside class="sidebar">
+        <div class="sidebar-section">
+            <h3 class="sidebar-title">Navegación Rápida</h3>
+            <ul class="sidebar-menu">
+                <li><a href="/controllers/HomeController.php?accion=homePaciente">🏠 Inicio</a></li>
+                <li><a href="#">📆 Tus Citas</a></li>
+                <li><a href="/controllers/HomeController.php?accion=PacientePerfilView&id_usuario=<?php echo $_SESSION['usuario']['id_usuario']; ?>">👤 Tu Perfil</a></li>
+                <li><a href="/views/paciente/historial/historial_dashboard.php">🏥 Historial Clínico</a></li>
+                <li><a href="/controllers/PqrsController.php?accion=pacientePqrs&id_usuario=<?php echo $_SESSION['usuario']['id_usuario']; ?>" class="active">⚙️ Mis Pqrs</a></li>
+            </ul>
+            <a href="/controllers/AuthController.php?accion=Logout" class="btn btn-primary" style="margin-top: 16px;">Cerrar Sesion</a>
+        </div>
+
+    </aside>
