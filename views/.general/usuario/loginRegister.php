f5c7a52ec4b036ba7da4524ccc085f0379c22199
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-08-29 10:20:40 -0500

    Login CSS

--- views/.general/usuario/loginRegister.php
+++ views/.general/usuario/loginRegister.php
@@ -9,15 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <?php
-    $cssPath = $_SERVER['DOCUMENT_ROOT'] . '/assets/css/user/loginRegister.css';
-    $cssUrl = '/assets/css/user/loginRegister.css';
-    if (file_exists($cssPath)) {
-        echo '<link rel="stylesheet" href="' . $cssUrl . '">';
-    } else {
-        echo ' CSS File not fount at: ' . $cssPath . '';
-    }
-    ?>
+
     <link rel="stylesheet" href="/assets/css/user/loginRegister.css?v=20250830">
 </head>
 <body>
