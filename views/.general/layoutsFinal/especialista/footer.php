917a3d294b31d539268b19b5606f7bc22ff996da
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-04 06:48:18 -0500

    Congruencia Rutas

--- views/.general/layoutsFinal/especialista/footer.php
+++ views/.general/layoutsFinal/especialista/footer.php
@@ -4,9 +4,9 @@
                 © 2025 SAB. Todos los derechos reservados.
             </div>
             <div class="footer-links">
-                <a href="/views/especialista/terminosEspecialista.php">Términos y condiciones</a>
-                <a href="/views/especialista/sobreEspecialista.php">Sobre Nosotros</a>
-                <a href="/views/especialista/soporteEspecialista.php">Soporte</a>
+                <a href="/views/especialista/extras/terminosEspecialista.php">Términos y condiciones</a>
+                <a href="/views/especialista/extras/sobreEspecialista.php">Sobre Nosotros</a>
+                <a href="/views/especialista/extras/soporteEspecialista.php">Soporte</a>
             </div>
         </div>
     </footer>
