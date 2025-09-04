ba9fffda3f74d107abb32640b1b4134b7c18391b
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-04 15:34:40 -0500

    Changes For Citas Agendar

--- views/paciente/cita/citaAgendar1.php
+++ views/paciente/cita/citaAgendar1.php
@@ -11,6 +11,8 @@ $id_usuario = $_SESSION['usuario']['id_usuario'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendar Cita</title>
+    <!-- CSS de Select2 -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="/Assets/css/layoutFinal/paciente/layout1.css">
     <link rel="stylesheet" href="/assets/css/citas/citasAgendar1.css?v=20250906">
@@ -143,6 +145,15 @@ $id_usuario = $_SESSION['usuario']['id_usuario'];
     ?>
 
     <script src="/assets/js/cita/citaAgendar.js"></script>
+    <!-- JS de jQuery y Select2 -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <!-- <script src="script.js"></script> -->
+    <script>
+$(document).ready(function() {
+    $('#cita_especialista').select2();
+});
+</script>
 </body>
 
 </html>
