78f4f3d2fb68875f879e2cbd4a902ebfde233b86
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-04 15:45:50 -0500

    Changes For Citas Agendar

--- views/paciente/cita/citaAgendar1.php
+++ views/paciente/cita/citaAgendar1.php
@@ -55,7 +55,7 @@ $id_usuario = $_SESSION['usuario']['id_usuario'];
                                 <label for="cita_especialista">Especialista <span class="required">*</span></label>
                                 <div class="input-with-icon">
                                     <i class="fas fa-user-doctor input-icon"></i>
-                                    <select name="cita_especialista" id="cita_especialista" class="select2-selection select2-selection--single" required>
+                                    <select name="cita_especialista" id="cita_especialista" class="form-control select2" required>
                                         <option value="">Seleccionar especialista...</option>
                                         <?php foreach ($especialistas as $especialista): ?>
                                             <option value="<?= $especialista['id_especialista'] ?>">
