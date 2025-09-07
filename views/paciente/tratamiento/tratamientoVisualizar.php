8667bfdf6fa5d623c64c5340c2528833c22ec1ed
Angelo gonzalez <4198126@gmail.com>
2025-09-07 11:24:01 -0500

    modal

--- views/paciente/tratamiento/tratamientoVisualizar.php
+++ views/paciente/tratamiento/tratamientoVisualizar.php
@@ -117,7 +117,7 @@
             <p><strong>Estado:</strong> <span id="modal-estado"></span></p>
 
             <div class="modal-footer">
-                <a href="/views/paciente/cita/agendarCita1.php" class="btn-agendar">Agendar Cita</a>
+                <a href="/views/paciente/cita/citaAgendar1.php" class="btn-agendar">Agendar Cita</a>
             </div>
         </div>
     </div>
