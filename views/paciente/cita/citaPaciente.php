321c414d91394c799ea01aaa78178a757a5849ec
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-08 10:52:01 -0500

    Changes Cita

--- views/paciente/cita/citaPaciente.php
+++ views/paciente/cita/citaPaciente.php
@@ -31,9 +31,9 @@ $nombreUsuario = $_SESSION['usuario']['usua_nombre'];
 </head>
 <body>
     <?php 
-        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/header.php');
-        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/nav/navCita.php');
-        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/aside/asideCita.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/paciente/header.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/paciente/nav/nav.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/paciente/aside/asideTusCitas.php');
     ?>
     <main class="main-content">
             <h2 class="content-title">Tus Citas Pendientes</h2>
@@ -66,7 +66,7 @@ $nombreUsuario = $_SESSION['usuario']['usua_nombre'];
         </table>
     </main>
     <?php
-        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/footer.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/paciente/footer.php');
     ?>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
