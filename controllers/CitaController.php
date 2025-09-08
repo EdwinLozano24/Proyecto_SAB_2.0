d0e0ff8d2687441e991be98bbfc89d203c11ff47
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-08 10:49:45 -0500

    Changes Cita

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -424,7 +424,6 @@ class CitaController
     {
         $id_paciente = $this->PacienteModel->findIdPaciente($id_usuario);
         $cita = $this->CitaModel->findCitaPaciente($id_paciente);
-        var_dump($cita); exit;
         include '../views/paciente/cita/citaPaciente.php';
     }
 }
