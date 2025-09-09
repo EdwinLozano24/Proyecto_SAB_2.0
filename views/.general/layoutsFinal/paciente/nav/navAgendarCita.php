3183a4c1ca35a6bc0512bd7c972a8cc3eb8ca2be
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-09 14:48:55 -0500

    Changes Rutas Admin

--- views/.general/layoutsFinal/paciente/nav/navAgendarCita.php
+++ views/.general/layoutsFinal/paciente/nav/navAgendarCita.php
@@ -8,7 +8,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="/controllers/CitaController.php?accion=viewAgendar" class="nav-link active">
+                <a href="/controllers/CitaController.php?accion=viewAgendar&rol='Paciente'" class="nav-link active">
                     <div class="nav-icon"></div>
                     Agenda tu Cita
                 </a>
