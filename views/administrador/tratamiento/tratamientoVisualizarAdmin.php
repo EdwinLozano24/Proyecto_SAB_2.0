e4456a99d42ff629ad95bfa0df1130edbdcf6618
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-09 14:06:59 -0500

    Changes Rutas Admin

--- views/administrador/tratamiento/tratamientoVisualizarAdmin.php
+++ views/administrador/tratamiento/tratamientoVisualizarAdmin.php
@@ -117,7 +117,7 @@
             <p><strong>Estado:</strong> <span id="modal-estado"></span></p>
 
             <div class="modal-footer">
-                <a href="/views/paciente/cita/citaAgendar1.php" class="btn-agendar">Agendar Cita</a>
+                <a href="/views/administrador/cita/citaAgendarAdmin.php" class="btn-agendar">Agendar Cita</a>
             </div>
         </div>
     </div>
