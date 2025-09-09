0667bf947c116e8c9d7a3fbc99934bf0998939c2
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-09 12:31:26 -0500

    Changes Rutas Admin

--- views/.general/layoutsFinal/admin/nav/nav.php
+++ views/.general/layoutsFinal/admin/nav/nav.php
@@ -8,13 +8,13 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="/controllers/CitaController.php?accion=pacienteAgendar" class="nav-link">
+                <a href="/controllers/CitaController.php?accion=viewAgendar" class="nav-link">
                     <div class="nav-icon"></div>
                     Agenda tu Cita
                 </a>
             </li>
             <li class="nav-item">
-                <a href="<?php echo dirname(dirname($_SERVER['SCRIPT_NAME'])) ?>/views/tratamiento/tratamientoVisualizar.php" class="nav-link">
+                <a href="/views/paciente/tratamiento/tratamientoVisualizar.php" class="nav-link">
                     <div class="nav-icon"></div>
                     Tratamientos Disponibles
                 </a>
