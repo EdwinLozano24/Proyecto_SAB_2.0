c80ee1a4164bd5908eb3a2e7647394b4ab942fce
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-09 13:39:17 -0500

    Changes Rutas Admin

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -321,6 +321,7 @@ class CitaController
     public function viewAgendar($rol)
     {
         $especialistas = $this->EspecialistaModel->findAll();
+        var_dump($rol); exit;
         if($rol === "Administrador")
         {
             include '../views/administrador/cita/citaAgendar.php';
