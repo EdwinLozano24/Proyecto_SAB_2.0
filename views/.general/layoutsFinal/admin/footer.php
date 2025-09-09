1816037b4e4b3c3407f7baca1fafeb11c9628697
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-09 14:33:58 -0500

    Changes Rutas Admin

--- views/.general/layoutsFinal/admin/footer.php
+++ views/.general/layoutsFinal/admin/footer.php
@@ -4,9 +4,9 @@
                 © 2025 SAB. Todos los derechos reservados.
             </div>
             <div class="footer-links">
-                <a href="/views/admin/extras/terminosCondiciones.php">Términos y condiciones</a>
-                <a href="/views/admin/extras/sobreNosotros.php">Sobre Nosotros</a>
-                <a href="/views/admin/extras/soporte.php">Soporte</a>
+                <a href="/views/administrador/extras/terminosCondiciones.php">Términos y condiciones</a>
+                <a href="/views/administrador/extras/sobreNosotros.php">Sobre Nosotros</a>
+                <a href="/views/administrador/extras/soporte.php">Soporte</a>
             </div>
         </div>
     </footer>
