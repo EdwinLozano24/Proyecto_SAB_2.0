08109450da48ffa5c98123bc995c99cb84d39cb4
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-10 02:14:43 -0500

    Changes Rutas Admin

--- views/especialista/home/especialista_dashboard.php
+++ views/especialista/home/especialista_dashboard.php
@@ -1,8 +1,10 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config/auth.php';
+require_once '../../../config/auth.php';
+
 requiereSesion();
 $id_usuario = $_SESSION['usuario']['id_usuario'];
 $nombreUsuario = $_SESSION['usuario']['usua_nombre'];
+
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +13,7 @@ $nombreUsuario = $_SESSION['usuario']['usua_nombre'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../Assets/css/layoutFinal/admin/layout1.css?v=20250905">
+    <link rel="stylesheet" href="/Assets/css/layoutFinal/paciente/layout1.css">
     <title>¡Bienvenido a SAB!</title>
 </head>
 
@@ -26,59 +28,33 @@ $nombreUsuario = $_SESSION['usuario']['usua_nombre'];
     <main class="main-content">
         <div class="content-header">
             <h2 class="content-title">Bienvenido a SAB</h2>
-            <p class="content-subtitle">Panel de administración</p>
+            <p class="content-subtitle">Explora las diferentes secciones y descubre todo lo que tenemos para ofrecer como sistema de gestión odontológica.</p>
         </div>
-
         <div class="content-grid">
             <div class="content-card">
-                <h3 class="card-title">Gestionar usuarios</h3>
-                <p class="card-description"></p>
-                <a href="/controllers/UsuarioController.php?accion=index" class="btn btn-primary" style="margin-top: 16px;">Ir</a>
+                <h3 class="card-title">Agenda Tu Cita</h3>
+                <p class="card-description">Reserva fácilmente tu cita para chequeos, limpiezas, tratamientos especializados o cualquier consulta.
+                ¡Tu salud oral es nuestra prioridad!.</p>
+                <a href="/controllers/CitaController.php?accion=viewAgendar&rol=Paciente" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
             </div>
+
             <div class="content-card">
-                <h3 class="card-title">Gestionar citas</h3>
-                <p class="card-description"></p>
-                <a href="/controllers/CitaController.php?accion=index" class="btn btn-primary" style="margin-top: 16px;">Ir</a>
+                <h3 class="card-title">Tratamientos</h3>
+                <p class="card-description">En nuestro Centro Odontológico te ofrecemos una amplia gama de tratamientos diseñados para cuidar y mejorar tu salud bucal.</p>
+                <a href="/views/paciente/tratamiento/tratamientoVisualizar.php" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
+            </div>
+
+            <div class="content-card">
+                <h3 class="card-title">Crear PQR</h3>
+                <p class="card-description">En SAB estamos comprometidos con mejorar cada día.
+        Envía tus peticiones, quejas, reclamos o sugerencias y ayúdanos a brindarte una mejor atención.</p>
+                <a href="/views/paciente/pqr/crearPQR.php" class="btn btn-primary" style="margin-top: 16px;">Ver mas</a>
             </div>
         </div>
-
-        <div class="content-grid">
-            <div class="content-card">
-                <h3 class="card-title">Usuario activos</h3>
-                <p class="card-description">32 (ejemplo)</p>
-                <a href="#" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
-            </div>
-
-            <div class="content-card">
-                <h3 class="card-title">Resumen citas</h3>
-                <p class="card-description">18 (ejemplo)</p>
-                <a href="#" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
-            </div>
-
-            <div class="content-card">
-                <h3 class="card-title">PQRS registrados</h3>
-                <p class="card-description">25 (ejemplo)</p>
-                <a href="#" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
-            </div>
-        </div>
-
-        <div class="content-grid">
-            <div class="content-card">
-                <h3 class="card-title">Solucione PQRS</h3>
-                <p class="card-description"></p>
-                <a href="/controllers/PqrsController.php?accion=index" class="btn btn-primary" style="margin-top: 16px;">Ir</a>
-            </div>
-            <div class="content-card">
-                <h3 class="card-title">Solicitudes Historial Clínico</h3>
-                <p class="card-description">3 (ejemplo)</p>
-                <a href="#" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
-            </div>
     </main>
-
-    <?php
-    include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/footer.php');
-    ?>
-
+<?php 
+include($_SERVER['DOCUMENT_ROOT'] . '/views/.general/layoutsFinal/especialista/footer.php');
+?>
 </body>
 
 </html>
