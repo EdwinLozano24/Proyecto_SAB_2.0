9beb62545dc37801fa05441311846bbf2ebf3fc2
Angelo gonzalez <4198126@gmail.com>
2025-09-09 22:07:37 -0500

    Pqña ruta de dhasboard admin a consultorios

--- views/administrador/home/admin_dashboard.php
+++ views/administrador/home/admin_dashboard.php
@@ -56,19 +56,12 @@ $nombreUsuario = $_SESSION['usuario']['usua_nombre'];
                 <a href="#" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
             </div>
 
-            <div class="content-card">
-                <h3 class="card-title">Consultorios</h3>
-                <p class="card-description"></p>
-                <a href="/controllers/ConsultorioController.php?accion=index" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
-            </div>
-        </div>
-
         <div class="content-card">
             <h3 class="card-title">Solicitudes Historial Clínico</h3>
             <p class="card-description">3 (ejemplo)</p>
             <a href="#" class="btn btn-primary" style="margin-top: 16px;">Ver más</a>
         </div>
-        
+
         <div class="content-grid">
             <div class="content-card">
                 <h3 class="card-title">Solucione PQRS</h3>
