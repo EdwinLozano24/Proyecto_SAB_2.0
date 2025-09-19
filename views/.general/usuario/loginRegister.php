8efb14fd6162956776b104c96de6ef6cb90cd978
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-19 06:52:41 -0500

    Prueba Repositorio

--- views/.general/usuario/loginRegister.php
+++ views/.general/usuario/loginRegister.php
@@ -167,6 +167,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Script propio para alternar entre login/registro -->
-    <script src="/Assets/js/user/loginRespon.js"></script>
+    <script src="/assets/js/user/loginRespon.js"></script>
 </body>
 </html>
