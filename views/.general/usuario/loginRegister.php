e4eff7105a26b34ddfcf7237ea770f4c5869aeaf
edwinson <camix24k@gmail.com>
2025-09-19 09:14:38 -0500

    chamaquita

--- views/.general/usuario/loginRegister.php
+++ views/.general/usuario/loginRegister.php
@@ -15,7 +15,7 @@
 
 
     <!-- CSS personalizado (se fuerza actualización con versión al final) -->
-    <link rel="stylesheet" href="/assets/css/user/loginRegister.css?v=20250845">
+    <link rel="stylesheet" href="/assets/css/user/loginRegister.css?v=20250884">
 </head>
 <body>
     <?php 
