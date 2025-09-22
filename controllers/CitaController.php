35bcf8cae1f444e793c031d801665128cb900253
SantiagoCC <santiagocarranzacarrillo@gmail.com>
2025-09-22 18:51:48 -0500

    change prueba

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -182,7 +182,7 @@ class CitaController
         $especialista = $this->EspecialistaModel->findEspecialista($id_usuario);
         if (!$especialista) {
             $errors[] = "Pacere que tienes el rol de especialista, pero no esta ingresado internamente, comunicate con un administrador.";
-            include '../views/.general/error/alertaIndex.php';
+            include '../views/.general/error/alertaEspe.php';
             exit;
         }
         $id_especialista = $especialista['id_especialista'];
